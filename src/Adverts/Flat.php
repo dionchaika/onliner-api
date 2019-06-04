ad2294eafd59e3c12f9fcb7783f1c7b9b60a2038
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-04 17:48:29 +0300

    Update Flat.php

--- src/Adverts/Flat.php
+++ src/Adverts/Flat.php
@@ -22,7 +22,67 @@ class Flat implements AdvertInterface
      */
     protected $data = [
 
-        
+        'area' => [
+
+            'total'   => null,
+            'living'  => null,
+            'kitchen' => null
+
+        ],
+
+        'building_year' => null,
+
+        'contact' => [
+
+            'call_time' => [9, 18],
+            'name'      => null,
+            'phones'    => []
+
+        ],
+
+        'description'      => null,
+        'facilities'       => [],
+        'floor'            => null,
+        'number_of_floors' => null,
+        'id'               => null,
+
+        'location' => [
+
+            'address'      => null,
+            'user_address' => null,
+            'latitude'     => null,
+            'longitude'    => null
+
+        ],
+
+        'parking' => null,
+        'photos'  => [],
+
+        'price' => [
+
+            'amount'   => null,
+            'currency' => null
+
+        ],
+
+        'resale'          => null,
+        'number_of_rooms' => null,
+
+        'seller' => [
+
+            'type' => null,
+            'name' => null,
+
+            'legal_info' => [
+
+                'unp' => null
+
+            ]
+
+        ],
+
+        'wall_height' => null,
+        'walling'     => null
 
     ];
 }
