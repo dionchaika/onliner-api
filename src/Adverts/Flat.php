77bfdb908f6290fef1b98c1b02e2aaad0eca06c3
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-06-04 17:08:10 +0300

    Add advert

---
+++ src/Adverts/Flat.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace API\Onliner\Adverts;
+
+use API\Kufar\Finder;
+use Dionchaika\Http\Uri;
+use InvalidArgumentException;
+use API\Kufar\AdvertInterface;
+use Psr\Http\Message\RequestInterface;
+use Dionchaika\Http\Factory\RequestFactory;
+
+/**
+ * The flat advert class.
+ */
+class Flat implements AdvertInterface
+{
+    /**
+     * The array
+     * of flat advert data.
+     *
+     * @var mixed[]
+     */
+    protected $data = [
+
+        
+
+    ];
+}
---
+++ src/Finder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace API\Onliner;
+
+/**
+ * The selections helper class.
+ */
+abstract class Finder
+{
+    /**
+     * Suggest an array key.
+     *
+     * @param  string  $query
+     * @param  mixed[]  $suggestions
+     *
+     * @return mixed
+     */
+    public static function suggestKey(string $query, array $suggestions)
+    {
+        $query = trim(mb_strtolower($query));
+
+        $map = [];
+        foreach ($suggestions as $key => $value) {
+            if ($query === $value) {
+                return $key;
+            }
+
+            similar_text($query, trim(mb_strtolower($value)), $percent);
+            $map[$key] = $percent;
+        }
+
+        return array_search(max(array_values($map)), $map);
+    }
+
+    /**
+     * Suggest an array value.
+     *
+     * @param  string  $query
+     * @param  mixed[]  $suggestions
+     *
+     * @return string
+     */
+    public static function suggestValue(string $query, array $suggestions)
+    {
+        $query = trim(mb_strtolower($query));
+
+        $map = [];
+        foreach ($suggestions as $key => $value) {
+            if ($query === $value) {
+                return $value;
+            }
+
+            similar_text($query, trim(mb_strtolower($value)), $percent);
+            $map[$key] = $percent;
+        }
+
+        return $suggestions[array_search(max(array_values($map)), $map)];
+    }
+}
